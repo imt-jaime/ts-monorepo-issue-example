8a899d54d43a2ff7de4601dfd3d5477973330219
jaime_brito <jaime.brito@intermodaltelematics.com>
2020-01-31 13:39:02 +0000

    update-repository-to-typescript-3-7-5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ts_issue_module_b_1 = require("ts-issue-module-b");
-// import { SomeInterface } from 'ts-typelib-example'; this line makes no sense
+// import { SomeInterface } from 'ts-typelib-example'; // this line makes no sense
 const valueB = ts_issue_module_b_1.moduleB(3);
 exports.valueB = valueB;
